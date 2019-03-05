d6a00ac72f20c487df8e4a18412e4ca7ad73b66f
niklascote <cten@student.chalmers.se>
2019-03-05 16:59:56 +0100

    Fixed AuthController

--- WebApplicationProject/src/main/java/com/WebApplicationProject/control/AuthController.java
+++ WebApplicationProject/src/main/java/com/WebApplicationProject/control/AuthController.java
@@ -6,83 +6,53 @@
 package com.WebApplicationProject.control;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author niklascote
  */
-@WebServlet(name = "AuthController", urlPatterns = {"/AuthController"})
+@WebServlet(name = "Auth", urlPatterns = {"/Auth"})
 public class AuthController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AuthController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AuthController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String user = request.getParameter("user"); //Fetches data from loginView.xhtml
+        String pass = request.getParameter("pass");
+        
+        try{
+            Class.forName("com.derby.jdbc.Driver");
+            Connection c = DriverManager.getConnection("jdbc:derby://localhost:1527/scheduleDatabase\", \"root\", \"root");
+            PreparedStatement ps = c.prepareStatement("select FIRSTNAME,PASSWORD from USERS where FIRSTNAME=? and PASSWORD=?");
+            ps.setString(1, user); //Currently uses FIRSTNAME as User
+            ps.setString(2, pass);
+ 
+            ResultSet rs = ps.executeQuery();
+ 
+            while (rs.next()) {
+                HttpSession session =request.getSession();
+                session.setAttribute("user", user);
+                response.sendRedirect("schedule/scheduleView.xhtml"); //Direction if login is successful (PLACEHOLDER)
+		return;
+            }
+            response.sendRedirect("schedule/loginView.xhtml");//Direction if login is NOT successful (PLACEHOLDER)
+        }
+        catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(AuthController.class.getName()).log(Level.SEVERE, null, ex);
+        }   
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
